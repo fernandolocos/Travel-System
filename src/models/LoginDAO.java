ffa5b866cff08b3bf88a615dfce5d27c63502de5
oscaresgalha@gmail.com <oscaresgalha@gmail.com@17501ddd-b8a6-a788-90d8-ec16196fc182>
2012-06-14 23:34:41 +0000

    Login

---
+++ src/models/LoginDAO.java
@@ -0,0 +1,13 @@
+package models;
+
+import enums.EnumLogin;
+
+public class LoginDAO {
+	
+	
+	public static EnumLogin logar(String usuario, String senha) {
+		return EnumLogin.Sucesso;
+	}
+	
+	
+}
---
+++ src/views/LayoutPrincipal.java
@@ -0,0 +1,20 @@
+package views;
+
+import enums.EnumLogin;
+
+public class LayoutPrincipal {
+
+	public final static LayoutPrincipal INSTANCE = new LayoutPrincipal();
+	
+	@SuppressWarnings("unused")
+	private FormularioLogin formularioLogin;
+	
+	public void atualizarPaginaLogou() {
+		
+	}
+	
+	public void mostrarErroLogin(EnumLogin erro) {
+		
+	}
+	
+}
