3b2321264eea01f7efe81bc87a3d081f462879a8
fernandolocos@gmail.com <fernandolocos@gmail.com@17501ddd-b8a6-a788-90d8-ec16196fc182>
2012-06-18 23:53:50 +0000

    diagrama alterado

---
+++ trunk/src/controllers/UsuarioController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+public class UsuarioController {
+	public void efetuarCadastroAction()
+	{
+		
+	}
+	
+	public void alterarDadosCadastraisAction()
+	{
+		
+	}
+	
+	public void efetuarLogin()
+	{
+		
+	}
+	
+	public void recuperarSenhaAction()
+	{
+		
+	}
+}
