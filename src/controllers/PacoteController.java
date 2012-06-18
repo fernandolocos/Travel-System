81227e7fa48538e7d5c479c3ae9e3999b2e3c4c8
fernandolocos@gmail.com <fernandolocos@gmail.com@17501ddd-b8a6-a788-90d8-ec16196fc182>
2012-06-18 23:53:50 +0000

    diagrama alterado

---
+++ src/controllers/PacoteController.java
@@ -0,0 +1,63 @@
+package controllers;
+
+public class PacoteController {
+	public void comprarPacoteViagemAction()
+	{
+		
+	}
+	
+	public void montarPacoteViagemAction()
+	{
+		
+	}
+	
+	public void escolherPacoteViagemMaritimaAction()
+	{
+		
+	}
+	
+	public void visualizarRoteiroViagemAction()
+	{
+		
+	}
+	
+	public void imprimirRoteiroViagemAction()
+	{
+		
+	}
+	
+	public void escolherHotelAction()
+	{
+		
+	}
+	
+	public void confirmarCompraPacoteAction()
+	{
+		
+	}
+	
+	public void escolherGuiasAction()
+	{
+		
+	}
+	
+	public void escolherPasseiosAction()
+	{
+		
+	}
+	
+	public void cancelarPacoteAction()
+	{
+		
+	}	
+	
+	public void escolherDestinoAction()
+	{
+		
+	}	
+	
+	public void escolherLocalPartidaAction()
+	{
+		
+	}	
+}
--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -1,5 +1,23 @@
 package models;
 
-public class Usuario {
+import views.LayoutPrincipal;
+import enums.EnumLogin;
 
+public class Usuario {
+	// viajarfacil.com/logon/logar
+	public void logar(String usuario, String senha) {
+		LoginDAO.logar(usuario, senha);
+		loginSucesso();
+		mostrarErro(EnumLogin.DadosErrados);
+	}
+	
+	// Atualizar a página
+	private void loginSucesso() {
+		LayoutPrincipal.INSTANCE.atualizarPaginaLogou();
+	}
+	
+	// Mostrar 
+	private void mostrarErro(EnumLogin erro) {
+		LayoutPrincipal.INSTANCE.mostrarErroLogin(erro);
+	}
 }
