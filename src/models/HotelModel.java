2fa20f5fff13d3515523e9b7cd40ebe1e74e83b6
oscaresgalha@gmail.com <oscaresgalha@gmail.com@17501ddd-b8a6-a788-90d8-ec16196fc182>
2012-06-21 17:50:46 +0000

    hotel

--- src/models/HotelModel.java
+++ src/models/HotelModel.java
@@ -1,5 +1,54 @@
 package models;
 
-public class HotelModel {
+import java.util.List;
 
+@SuppressWarnings("unused")
+public class HotelModel extends ModelBase {
+	
+	private final static String ID = "ID";
+	private final static String IMAGENS = "IMG";
+	private final static String ENDERECO = "END";
+	private final static String ESTRELA = "STAR";
+	private final static String DESCRICAO = "DESC";
+	
+	public void setHotelID(Long ID) {
+		
+	}
+	
+	public Long getHotelID() {
+		return null;
+		
+	}
+	
+	public void setHotelImages(List<String> URLS) {
+		
+	}
+	
+	public List<String> getHotelImages() {
+		return null;
+	}
+	
+	public void setHotelEndereco(Endereco endereco) {
+		
+	}
+	
+	public Endereco getHotelEndereco() {
+		return null;
+	}
+	
+	public void setEstrelas(int estrelas) {
+		
+	}
+	
+	public int getEstrelas() {
+		return 0;
+	}
+	
+	public void setDescricao(String descricao) {
+		
+	}
+	
+	public String getDescricao() {
+		return null;
+	}
 }
