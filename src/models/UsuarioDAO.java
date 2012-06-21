422409a7caa08f16bc646b68a5afd87130233991
oscaresgalha@gmail.com <oscaresgalha@gmail.com@17501ddd-b8a6-a788-90d8-ec16196fc182>
2012-06-21 19:17:37 +0000

    XUBLANGS

---
+++ src/models/UsuarioDAO.java
@@ -0,0 +1,24 @@
+package models;
+
+import enums.EnumLogin;
+
+public class UsuarioDAO {
+	
+	public EnumLogin logar(String usuario, String senhaSalgada) {
+		return EnumLogin.Sucesso;
+	}
+	
+	public String gerarNovaSenha(Long ID) {
+		return null;
+	}
+	
+	public boolean alterarDadosCadastrais(String nome, String email, String senha, 
+			String RG, String CPF, int idade) {
+		return false;
+	}
+
+	public boolean cadastrar(String nome, String senha, String sal, String email, 
+			String RG, String CPF, int idade) {
+		return false;
+	}
+}
--- src/models/UsuarioModel.java
+++ src/models/UsuarioModel.java
@@ -2,21 +2,66 @@ package models;
 
 import enums.EnumLogin;
 
-public class UsuarioModel {
-	// viajarfacil.com/logon/logar
-	public void logar(String usuario, String senha) {
-		LoginDAO.logar(usuario, senha);
-		loginSucesso();
-		mostrarErro(EnumLogin.DadosErrados);
-	}
+@SuppressWarnings("unused")
+public class UsuarioModel extends PassageiroModel {
 	
-	// Atualizar a página
-	private void loginSucesso() {
+	private final static String ID = "ID";
+	private final static String SENHA = "PASS";
+	private final static String SALT = "Sal";
+	private final static String EMAIL = "EM";
 	
-	}
+	private UsuarioDAO dao;
 	
-	// Mostrar 
-	private void mostrarErro(EnumLogin erro) {
+	public void setID(Long ID) {
 		
 	}
+	
+	public Long getID() {
+		return null;
+	}
+	
+	public void setSenha(String senha) {
+		
+	}
+	
+	public String getSenha() {
+		return null;
+	}
+	
+	public void setSal(String sal) {
+		
+	}
+	
+	public String getSal() {
+		return null;
+	}
+	
+	public void setEmail(String email) {
+		
+	}
+	
+	public String getEmail() {
+		return null;
+	}
+	
+	public EnumLogin logar(String usuario, String senhaSalgada) {
+		return null;
+	}
+	
+	private String salgarSenha(String senha, String sal) {
+		return null;
+	}
+	
+	public boolean alterarDadosCadastrais() {
+		return false;
+	}
+	
+	public boolean gerarNovaSenha() {
+		return false;
+	}
+	
+	public boolean cadastrar() {
+		return false;
+	}
+	
 }
